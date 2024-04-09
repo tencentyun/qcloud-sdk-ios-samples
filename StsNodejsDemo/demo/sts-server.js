9cc126319b19eb72b45f44bc05b9de16feb9d869
garenwang <garenwang@tencent.com>
2024-04-09 15:48:10 +0800

    修改权限示例

--- StsNodejsDemo/demo/sts-server.js
+++ StsNodejsDemo/demo/sts-server.js
@@ -17,17 +17,9 @@ var config = {
     bucket: 'test-1250000000',
     region: 'ap-guangzhou',
     allowPrefix: 'exampleobject', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
-    // 简单上传和分片，需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
     allowActions: [
-        // 简单上传
-        'name/cos:PutObject',
-        'name/cos:PostObject',
-        // 分片上传
-        'name/cos:InitiateMultipartUpload',
-        'name/cos:ListMultipartUploads',
-        'name/cos:ListParts',
-        'name/cos:UploadPart',
-        'name/cos:CompleteMultipartUpload'
+        // 'name/cos:*'允许所有操作，仅作为测试使用，实际请按需分配权限，权限列表请看 https://cloud.tencent.com/document/product/436/31923
+        'name/cos:*',
     ],
 };
 
